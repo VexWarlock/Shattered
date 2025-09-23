58e1784bb6ec37bed394c37c4be122bf153d0d9a
Stanulet Sebastian <vexorwarlock@gmail.com>
2025-09-23 23:36:55 +0300

    v 1.03

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 local socket = require("socket")
-local server = assert(socket.bind("*", 4342))
+local server = assert(socket.bind("*", 4342)) -- schimbă portul pe Render dacă e nevoie
 server:settimeout(0)
 
 local clients = {}
@@ -11,29 +11,32 @@ local SECRET_TOKEN = "mysecret123"
 print("Server started on port 4342...")
 
 while true do
+    -- Acceptăm clienți noi
     local client = server:accept()
     if client then
         client:settimeout(0)
-        -- marcați clientul ca "neautentificat" la început
         clients[client] = { authenticated = false }
         print("Player connected! Waiting for authentication...")
     end
 
+    -- Procesăm clienții existenți
     for c, data in pairs(clients) do
         local line, err = c:receive("*l")
         if line then
             if not data.authenticated then
-                -- verificăm token-ul
+                -- Verificăm token-ul
                 if line == SECRET_TOKEN then
                     data.authenticated = true
                     print("Player authenticated!")
+                    -- Trimitem confirmarea clientului
+                    c:send("OK\n")
                 else
                     print("Authentication failed. Disconnecting client.")
                     c:close()
                     clients[c] = nil
                 end
             else
-                -- clientul este autentificat, procesăm poziția
+                -- Clientul e autentificat, procesăm poziția
                 local x, y = line:match("([^,]+),([^,]+)")
                 if x and y then
                     positions[c] = { x = tonumber(x), y = tonumber(y) }
